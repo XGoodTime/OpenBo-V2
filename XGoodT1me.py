515984fca0c762b62a10ead223166eb8bd2ae98b
XGT <89965330+XGoodTime@users.noreply.github.com>
2021-09-05 12:58:49 +0800

    Add files via upload

---
+++ XGoodT1me.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import random
+import socket
+import threading
+        print       (" - - > DDOS AMPE MAMPUS < - - ")%(sent, victim, vport)
+        print       (" - - > DDOS AMPE MAMPUS < - - ")%(sent, victim, vport)
+        print       (" - - > DDOS AMPE MAMPUS < - - ")%(sent, victim, vport) 
+        print       (" - - > DDOS AMPE MAMPUS < - - ")%(sent, victim, vport)
